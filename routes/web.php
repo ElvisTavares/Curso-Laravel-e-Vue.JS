c73e6f2b084607a27d5ae8f8c4ed414d452aceec
Elvis <elvisptavares@gmail.com>
2021-12-16 12:16:53 -0300

    Trabalhando con blade

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'index'])->name('site.index');
 Route::get('/sobre', [\App\Http\Controllers\SobreNosController::class, 'sobre'])->name('site.sobre');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function(){
